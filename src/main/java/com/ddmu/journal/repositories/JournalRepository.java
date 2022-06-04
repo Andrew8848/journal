48e167e7f423dd4f6f6ff9a34380b36c57e95c60
Andrew8848 <andrey.4ery@gmail.com>
2022-06-04 17:33:05 +0300

    changed utf8mb4 collate '=' on general '=' to avoid illegal mix of collactions

--- src/main/java/com/ddmu/journal/repositories/JournalRepository.java
+++ src/main/java/com/ddmu/journal/repositories/JournalRepository.java
@@ -18,13 +18,13 @@ public interface JournalRepository extends JpaRepository<Journal, Long> {
     @Query(value="SELECT * FROM journal WHERE journal.id COLLATE utf8mb4_unicode_ci =  :id", nativeQuery = true)
     Journal getById(@Param("id") Long id);
 
-    @Query(value = "SELECT COUNT(*) FROM journal INNER JOIN journal_status ON journal.journal_status_id COLLATE utf8mb4_unicode_ci =  journal_status.id WHERE journal_status.value COLLATE utf8mb4_unicode_ci = :journal_status", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM journal INNER JOIN journal_status ON journal.journal_status_id = journal_status.id WHERE journal_status.value COLLATE utf8mb4_unicode_ci = :journal_status", nativeQuery = true)
     long countJournalsByStatus(@Param("journal_status") String journalStatus);
 
-    @Query(value = "SELECT * FROM journal INNER JOIN journal_status ON journal.journal_status_id COLLATE utf8mb4_unicode_ci =  journal_status.id WHERE journal_status.value IN (:filter) ORDER BY journal.date_publication DESC, journal.time_publication DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM journal INNER JOIN journal_status ON journal.journal_status_id =  journal_status.id WHERE journal_status.value IN (:filter) ORDER BY journal.date_publication DESC, journal.time_publication DESC", nativeQuery = true)
     Page<Journal> findNewestJournalsByDateTimeOnPage(Pageable pageable, @Param("filter") List<String> filter);
 
-    @Query(value = "SELECT * FROM journal INNER JOIN doctor ON journal.doctor_id COLLATE utf8mb4_unicode_ci =  doctor.id INNER JOIN journal_status ON journal.journal_status_id COLLATE utf8mb4_unicode_ci = journal_status.id WHERE doctor.email IN (:emails) AND journal_status.value IN (:filter) ORDER BY journal.date_publication DESC, journal.time_publication DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM journal INNER JOIN doctor ON journal.doctor_id = doctor.id INNER JOIN journal_status ON journal.journal_status_id = journal_status.id WHERE doctor.email IN (:emails) AND journal_status.value IN (:filter) ORDER BY journal.date_publication DESC, journal.time_publication DESC", nativeQuery = true)
     Page<Journal> findNewestJournalsByEmailsAndDateTimeOnPage(Pageable pageable, @Param("emails") List<String> emails, @Param("filter") List<String> filter);
 
 
